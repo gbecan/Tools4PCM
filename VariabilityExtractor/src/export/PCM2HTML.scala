1c9baba7db5a21fc82f6a5c1e98fa78dde036a33
gbecan <guillaume.becan@irisa.fr>
2014-03-19 13:34:16 +0100

    HTML export of PCM model

--- VariabilityExtractor/src/export/PCM2HTML.scala
+++ VariabilityExtractor/src/export/PCM2HTML.scala
@@ -4,6 +4,7 @@ import pcmmm.PCM
 import scala.xml.Elem
 import scala.collection.JavaConversions._
 import pcmmm.Matrix
+import pcmmm.Cell
 
 class PCM2HTML {
 
@@ -22,9 +23,35 @@ class PCM2HTML {
   }
   
   def matrix2HTML(matrix : Matrix) : Elem = {
+    val cells = matrix.getCells()
+//    val sortedCells = cells.sortWith((c1,c2) => (
+//        c1.getRow() < c2.getRow()) 
+//        || ((c1.getRow() == c2.getRow()) && (c1.getColumn() < c2.getColumn()))
+//        )
+    val rows = cells.groupBy(cell => cell.getRow()).toList
+    val sortedRows = rows.sortBy(r => r._1).map(r => r._2)
+        
     val htmlCode = 
-    <table>
+    <table border="1">
+    		{for (row <- sortedRows) yield {
+    		  <tr> 
+    		  {for (cell <- row.sortBy(c => c.getColumn())) yield {
+    			  cell2HTML(cell)
+    		  }}
+    		  </tr>
+    		}}
     </table>
+    htmlCode
+  }
+  
+  def cell2HTML(cell : Cell) : Elem = {
+    val htmlCode = 
+    <td 
+    	rowspan={cell.getRowspan().toString}
+    	colspan={cell.getColspan().toString}
+    >
+    		{cell.getVerbatim()}
+    </td>
     htmlCode
   }
 }
