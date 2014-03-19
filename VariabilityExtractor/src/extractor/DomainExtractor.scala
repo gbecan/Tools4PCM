23eb7fe928d90ed6ba3ec44d70e461a4c620fc01
gbecan <guillaume.becan@irisa.fr>
2014-03-19 17:11:36 +0100

    Add domain collection to generated models

--- VariabilityExtractor/src/extractor/DomainExtractor.scala
+++ VariabilityExtractor/src/extractor/DomainExtractor.scala
@@ -1,12 +1,14 @@
 package extractor
 
 import pcmmm.PCM
+import pcmmm.PcmmmFactory
 
 class DomainExtractor {
 
   
   def extractDomains(pcm : PCM) {
-	  
+	  val domainCollection = PcmmmFactory.eINSTANCE.createDomainCollection()
+	  pcm.setDomainCollection(domainCollection)
   }
   
 }
